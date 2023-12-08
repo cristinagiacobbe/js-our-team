9629b0cd4b45e479b2d45178f4994f30c4f31d4a
cristinagiacobbe <crigiak@gmail.com>
2023-12-08 21:56:12 +0100

    Creat object (member team)

--- script.js
+++ script.js
@@ -15,7 +15,13 @@
 //mark up to convert strings into card
 
 
+const team = {
+    name: ["Wayne Barnett", "Angela Caroll", "Walter Gordon", "Angela Lopez", "Scott Estrada", "Barbara Ramos"],
+    role: ["Founder & CEO", "Chief Editor", "Office Manager", "Social Media Manager", "Developer", "Graphic Designer"],
+    photo: ["wayne-barnett-founder-ceo.jpg", "angela-caroll-chief-editor.jpg", "walter-gordon-office-manager.jpg", "angela-lopez-social-media-manager.jpg", "scott-estrada-developer.jpg", "barbara-ramos-graphic-designer.jpg"],
+}
 
+console.log(team);
 
 
 
