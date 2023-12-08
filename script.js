290f2e2f5e07f9f33a8b99b8968261c8868dec6c
cristinagiacobbe <crigiak@gmail.com>
2023-12-08 21:55:23 +0100

    Define tools

---
+++ script.js
@@ -0,0 +1,22 @@
+//MILESTONE 1:
+//Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+//MILESTONE 2:
+//Stampare le stesse informazioni su DOM sottoforma di stringhe
+//BONUS 1:
+//Trasformare la stringa foto in una immagine effettiva
+//BONUS 2:
+//Organizzare i singoli membri in card/schede
+
+//TOOLS
+//Object and array
+//document.innerHTML
+//loop (to repeat actions for every member team)
+//insert img
+//mark up to convert strings into card
+
+
+
+
+
+
+
