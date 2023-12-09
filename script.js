59de840d422cd4b7fb813eefe2ab4a420f89e2d3
cristinagiacobbe <crigiak@gmail.com>
2023-12-09 21:52:57 +0100

    Print on DOM a single member

--- script.js
+++ script.js
@@ -16,36 +16,36 @@
 
 
 const team = [
-{
-    name: "Wayne Barnett",
-    role: "Founder & CEO",
-    photo: "wayne-barnett-founder-ceo.jpg"
-},
-{
-    name: "Angela Caroll",
-    role: "Chief Editor",
-    photo: "angela-caroll-chief-editor.jpg"
-},
-{
-    name:"Walter Gordon", 
-    role: "Office Manager", 
-    photo: "walter-gordon-office-manager.jpg"
-},
-{
-    name: "Angela Lopez", 
-    role: "Social Media Manager", 
-    photo: "angela-lopez-social-media-manager.jpg"
-},
-{
-    name: "Scott Estrada", 
-    role:  "Developer",
-    photo: "scott-estrada-developer.jpg"
-},
-{
-    name: "Barbara Ramos",
-    role: "Graphic Designer",
-    photo: "barbara-ramos-graphic-designer.jpg"
-}
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        photo: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        photo: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        photo: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        photo: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        photo: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        photo: "barbara-ramos-graphic-designer.jpg"
+    }
 ]
 
 console.log(team.length);
@@ -53,8 +53,32 @@ console.log(team.length);
 for (let i = 0; i < team.length; i++) {
     const member = team[i];
     console.log(member);
+    
 }
 
+const member = team[0]
+console.log(member.role);
+
+
+const memberName = member.name
+const memberRole = member.role
+const memberPhoto = member.photo
+console.log(memberName);
+console.log(memberRole);
+console.log(memberPhoto);
+
+function MemberAppend(memberName) {
+const containerElement = document.querySelector(".container-card")
+const cardElement = document.createElement("div")
+cardElement.className = "card"
+cardElement.innerText = (memberName)
+containerElement.append(cardElement)    
+}
+
+MemberAppend(memberName)
+MemberAppend(memberRole)
+MemberAppend(memberPhoto)
+ 
 
 
 
